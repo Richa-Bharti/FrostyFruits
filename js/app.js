e48eea6619acab677992f055692e04a44a0499bc
Richa Bharti <54474191+Richa-Bharti@users.noreply.github.com>
2021-10-08 20:30:38 +0530

    Revert "Added preloader"

--- js/app.js
+++
@@ -1,6 +0,0 @@
-let spinnerWrapper = document.querySelector('.spinner-wrapper');
-window.addEventListener('load', function () {
-    // spinnerWrapper.style.display = 'none';
-    setTimeout(function(){ spinnerWrapper.parentElement.removeChild(spinnerWrapper);}, 2000);
-    // spinnerWrapper.parentElement.removeChild(spinnerWrapper);
-});

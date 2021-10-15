4d786c60be6b112f79be0daf54880e6eedece423
Archana Gandhi <74424757+Archana-gandhi@users.noreply.github.com>
2021-10-15 07:38:09 +0530

    fixed preloader

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 let spinnerWrapper = document.querySelector('.spinner-wrapper');
 window.addEventListener('load', function () {
-    // spinnerWrapper.style.display = 'none';
-    setTimeout(function(){ spinnerWrapper.parentElement.removeChild(spinnerWrapper);}, 2000);
-    // spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+    // setTimeout(function(){ spinnerWrapper.parentElement.removeChild(spinnerWrapper);}, 2000);
+    spinnerWrapper.parentElement.removeChild(spinnerWrapper);
 });

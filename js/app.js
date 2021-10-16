4f1184a9e69ac612f14643ef1cb6433bc5509073
Archana Gandhi <74424757+archanagandhi@users.noreply.github.com>
2021-10-16 08:37:40 +0530

    fixed changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let spinnerWrapper = document.querySelector('.spinner-wrapper');
 window.addEventListener('load', function () {
-    setTimeout(function(){ spinnerWrapper.parentElement.removeChild(spinnerWrapper);}, 500);
-    // spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+    // setTimeout(function(){ spinnerWrapper.parentElement.removeChild(spinnerWrapper);}, 500);
+    spinnerWrapper.parentElement.removeChild(spinnerWrapper);
 });
